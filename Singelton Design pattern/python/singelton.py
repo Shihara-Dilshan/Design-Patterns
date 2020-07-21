8a66e434ab9efe1fc58acc4c2f6e1705e8e5146d
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-07-21 14:08:33 +0800

    singelton.py

---
+++ Singelton Design pattern/python/singelton.py
@@ -0,0 +1,27 @@
+def singleton(cls):
+    instances = {}
+
+    def get_instance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+
+    return get_instance
+
+
+@singleton
+class Mine:
+    def __init__(self):
+        self.x = 12
+        print("object created")
+
+
+def main():
+    m = Mine()
+    www = Mine()
+    www = Mine()
+    www = Mine()
+
+
+if __name__ == '__main__':
+    main()
